b2aab851a9e31b958f68c0c8ec614f107dcab47b
HodayaBenAri <hodayaraz@gmail.com>
2022-04-11 14:31:44 +0300

    initial sign in page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 import ChatPage from './ChatPage/ChatPage.js';
+import BasePage from './registerPage/BasePage';
+import BasePage2 from './registerPage/BasePage2';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from 'react';
+
 
 function App() {
-  return(<ChatPage />
-  );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<BasePage2 />} />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
