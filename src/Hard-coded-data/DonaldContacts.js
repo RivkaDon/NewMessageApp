7e63aef09a2d5c49b3b2f5545119d9b72db68aee
RivkaDon <92295434+RivkaDon@users.noreply.github.com>
2022-04-25 17:58:49 +0300

    new changes after merge with register and sign in page

---
+++ src/Hard-coded-data/DonaldContacts.js
@@ -0,0 +1,9 @@
+import HarryPotterPic from './harryPotter.jpg'
+import HarryPmesseges from '../Hard-coded-data/Messages/HarryPmessages'
+import QueenEmessages from './Messages/QueenEmessages'
+import QueenElisabethPic from './queenElisabeth.jpg'
+
+
+const DonaldContacts = [{img: HarryPotterPic, name: 'Harry Potter', lastMessage: HarryPmesseges},
+{img: QueenElisabethPic, name: 'Queen Elisabeth', lastMessage: QueenEmessages}]
+export default DonaldContacts;
--- src/signIn/usersList.js
+++ src/signIn/usersList.js
@@ -1,5 +1,8 @@
-export const usersList = [{ username: 'harry', password: '1a', nickName:'Harry Potter', picture: "/profilePic.jpg"},
-{ username: 'Queen', password: '1b', nickName:'Queen Elisabeth', picture: "/profilePic.jpg"},
-{ username: 'Donald', password: '1c',nickName:'Donald Trump', picture: "/profilePic.jpg"}];
+import HarryContacts from "../Hard-coded-data/HarryContacts";
+import QueenContacts from "../Hard-coded-data/QueenContacts";
+import DonaldContacts from "../Hard-coded-data/DonaldContacts";
+export const usersList = [{ username: 'harry', password: '1a', nickName:'Harry Potter', picture: "/profilePic.jpg", contacts: HarryContacts},
+{ username: 'Queen', password: '1b', nickName:'Queen Elisabeth', picture: "/profilePic.jpg", contacts: QueenContacts},
+{ username: 'Donald', password: '1c',nickName:'Donald Trump', picture: "/profilePic.jpg", contacts: DonaldContacts}];
 
 export default usersList;
